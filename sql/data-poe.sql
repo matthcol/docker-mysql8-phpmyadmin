ac94798c59ea177a33683f02a4e0e0badfa262fb
matthcol <matthias.colin@gmail.com>
2023-01-04 10:51:16 +0100

    data trainee, poe example

---
+++ sql/data-poe.sql
@@ -0,0 +1,8 @@
+INSERT INTO poe (id, begin_date, end_date, title, poe_type) VALUES (1,'2022-10-24','2023-01-27','Java Fullstack','POEI');
+INSERT INTO poe (id, begin_date, end_date, title, poe_type) VALUES (2,'2022-11-02','2023-02-03','Java Fullstack','POEC');
+INSERT INTO poe (id, begin_date, end_date, title, poe_type) VALUES (3,'2022-06-13','2022-09-16','Consultant Devops','POEC');
+INSERT INTO poe (id, begin_date, end_date, title, poe_type) VALUES (4,'2021-09-13','2021-11-16','Consultant Cyber Sécurité','POEI');
+INSERT INTO poe (id, begin_date, end_date, title, poe_type) VALUES (5,'2022-04-13','2022-08-16','Consultant SAP','POEI');
+INSERT INTO poe (id, begin_date, end_date, title, poe_type) VALUES (6,'2022-09-24','2022-11-23','Consultant BI','POEI');
+INSERT INTO poe (id, begin_date, end_date, title, poe_type) VALUES (7,'2022-09-24','2022-11-23','Drôle de Consultant BI','POEI');
+
---
+++ sql/data-trainee.sql
@@ -0,0 +1,7 @@
+INSERT INTO trainee (id, birth_date, email, first_name, last_name, phone_number, poe_id) VALUES (1,'1950-11-07','john.doe@poe.org','John','Doe','+33700700700',1);
+INSERT INTO trainee (id, birth_date, email, first_name, last_name, phone_number, poe_id) VALUES (2,'1951-01-02','jane.doe@poe.org','Jane','Doe','+33700700701',1);
+INSERT INTO trainee (id, birth_date, email, first_name, last_name, phone_number, poe_id) VALUES (3,'1952-01-02','james.doe@poe.org','James','Doe','+33700700702',1);
+INSERT INTO trainee (id, birth_date, email, first_name, last_name, phone_number, poe_id) VALUES (4,'1953-10-07','han.solo@poe.org','Han','Solo','+33700700703',2);
+INSERT INTO trainee (id, birth_date, email, first_name, last_name, phone_number, poe_id) VALUES (5,'1954-08-12','jack.bauer@poe.org','Jack','Bauer','+33700700704',3);
+INSERT INTO trainee (id, birth_date, email, first_name, last_name, phone_number, poe_id) VALUES (7,'1955-11-24','elliot.read@poe.org','Elliot','Read','+33700700705',NULL);
+INSERT INTO trainee (id, birth_date, email, first_name, last_name, phone_number, poe_id) VALUES (8,'1959-10-24','ellen.ripley@poe.org','Ellen','Ripley','+33700700706',NULL);
